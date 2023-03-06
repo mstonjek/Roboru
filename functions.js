d4519235d7756350930f4fc4ea42ee95bc63e4ff
Slepicak007 <miggyczech@gmail.com>
2023-03-06 20:25:49 +0100

    buttons done

--- functions.js
+++ functions.js
@@ -1,8 +1,35 @@
-function scrollToSection(sectionId) {
-  const section = document.querySelector(sectionId);
-  window.scrollTo({
-    top: section.offsetTop,
-    behavior: "smooth",
-  });
-}
+(function ($) {
+  $(function () {
 
+
+    $(".contact").click(function () {
+      $("html, body").animate(
+        { scrollTop: $(".contact--scroll").offset().top },
+        1000
+      );
+    });
+
+    $(".date").click(function () {
+      $("html, body").animate(
+        { scrollTop: $(".date--scroll").offset().top },
+        1000
+      );
+    });
+
+    $(".homework").click(function () {
+      $("html, body").animate(
+        { scrollTop: $(".homework--scroll").offset().top },
+        1000
+      );
+    });
+
+   
+
+   
+
+
+    
+
+    
+  });
+})(jQuery);
